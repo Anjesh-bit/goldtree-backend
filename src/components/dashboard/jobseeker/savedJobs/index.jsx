397342ba24ae41f4ff45f70622fce4f40aebddcb
Anjesh-bit <75300382+Anjesh-bit@users.noreply.github.com>
2024-06-30 20:15:09 +0545

    Delete src directory
    chore(dir-delete):source directory deleted.

--- src/components/dashboard/jobseeker/savedJobs/index.jsx
+++
@@ -1,7 +0,0 @@
-import AppliedJobs from "../appliedJobs";
-
-const SavedJobs = () => {
-  return <AppliedJobs isSavedJobs />;
-};
-
-export default SavedJobs;
--- src/utils/expiration.js
+++
@@ -1,8 +0,0 @@
-//exp in minutes
-const getExpiration = (min) => {
-  const expiryDate = new Date();
-  expiryDate.setMinutes(expiryDate.getMinutes() + min);
-  return expiryDate;
-};
-
-export default getExpiration;
